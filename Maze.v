9d69c74b9a9f113c7c85f8f9f10c1e43f5ccc953
vlad-cool <vlad-matveev2004@mail.ru>
2024-11-07 14:55:54 +0300

    Spi working, but a bit slow

--- Maze.v
+++ Maze.v
@@ -2,6 +2,11 @@ module Maze(
 	input clk,
 	input rst,
 	
+	output wire LED1,
+	output wire LED2,
+	
+	output wire DEBUG_OUT1,
+	
 	output wire tft_rst,
 	output wire tft_clk,
 	output wire tft_mosi,
@@ -14,10 +19,20 @@ wire dc_out;
 wire transmit;
 wire spi_busy;
 
+wire tft_clk_in;
+
+clock_divider#(
+	.DIV_FACTOR(5)
+) clock_divider (
+	.global_reset(~rst),
+	.clk_in(clk),
+	.clk_out(tft_clk_in)
+);
+
 tft_spi spi_transmitter
 (
 	.global_reset(~rst),
-	.clk(clk),
+	.clk(tft_clk_in),
 	
 	.data(data_out),
 	.dc(dc_out),
@@ -34,19 +49,27 @@ tft_spi spi_transmitter
 
 reg [4:0]counter;
 
-assign transmt = counter == 2;
-assign data_out = 8'b10101010;
+reg [7:0]transmit_data;
+
+assign transmit = 1;
+// assign data_out = 8'b10000001;
+assign data_out = transmit_data;
 assign dc_out = 1;
 
-always @(posedge clk)
+assign DEBUG_OUT1 = transmit;
+
+always @(posedge tft_clk_in)
 begin
 	if (~rst)
 	begin
 		counter <= 0;
+		transmit_data <= 0;
 	end
 	else
 	begin
 		counter <= counter + 1;
+		
+		transmit_data <= counter == 0 ? transmit_data + 1 : transmit_data;
 		
 		// if (counter == 2 and ~spi_busy)
 		// begin
@@ -56,13 +79,7 @@ begin
 end
 
 /*
-clock_divider#(
-	.DIV_FACTOR(6)
-) clock_divider (
-	.global_reset(~rst),
-	.clk_in(clk),
-	.clk_out(tft_clk)
-);
+
 
 assign tft_mosi = clk;
 assign tft_cs = rst;
