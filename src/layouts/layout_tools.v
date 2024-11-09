462246ea969082d6620b449bd8db21b1f7fcffe7
VoyakaGOD <ab3000645@gmail.com>
2024-11-09 03:05:11 +0300

    layout_tools

---
+++ src/layouts/layout_tools.v
@@ -0,0 +1,103 @@
+//W = src width in "pixels"
+//H = src height in lines
+//S = size of "pixel" in bits
+//When you rotate layout: W(dst) = H(src) & H(dst) = W(src)
+
+module swap_v_layout
+#(
+    parameter W = 6,
+    parameter H = 6,
+    parameter S = 1
+)
+(
+    input   wire[(W*H*S - 1):0] src,
+    output  wire[(W*H*S - 1):0] dst
+);
+
+genvar i;
+generate
+    for (i = 0; i < H; i = i + 1) 
+    begin : loop
+        assign dst[(W*S*(i + 1) - 1):(W*S*i)] = src[(W*S*(H - i) - 1):(W*S*(H - i - 1))];
+    end
+endgenerate
+
+endmodule
+
+module swap_h_layout
+#(
+    parameter W = 6,
+    parameter H = 6,
+    parameter S = 1
+)
+(
+    input   wire[(W*H*S - 1):0] src,
+    output  wire[(W*H*S - 1):0] dst
+);
+
+genvar i, j;
+generate
+    for (i = 0; i < H; i = i + 1)
+    begin : row_loop
+        localparam line = i * W * S;
+        for (j = 0; j < W; j = j + 1)
+        begin : block_loop
+            assign dst[line + S * (j + 1) - 1 : line + (S * j)] = 
+                   src[line + S * (W - j) - 1 : line + (S * (W - j - 1))];
+        end
+    end
+endgenerate
+
+endmodule
+
+module rotate_l_layout
+#(
+    parameter W = 6,
+    parameter H = 6,
+    parameter S = 1
+)
+(
+    input   wire[(W*H*S - 1):0] src,
+    output  wire[(W*H*S - 1):0] dst
+);
+
+genvar i, j;
+generate
+    for (i = 0; i < H; i = i + 1)
+    begin : row_loop
+        for (j = 0; j < W; j = j + 1)
+        begin : block_loop
+            localparam ij = W - j - 1;
+            assign dst[(ij * H + i + 1) * S - 1 : (ij * H + i) * S] = 
+                   src[(i * W + j + 1) * S - 1 : (i * W + j) * S];
+        end
+    end
+endgenerate
+
+endmodule
+
+module rotate_r_layout
+#(
+    parameter W = 6,
+    parameter H = 6,
+    parameter S = 1
+)
+(
+    input   wire[(W*H*S - 1):0] src,
+    output  wire[(W*H*S - 1):0] dst
+);
+
+genvar i, j;
+generate
+    for (i = 0; i < H; i = i + 1)
+    begin : row_loop
+        for (j = 0; j < W; j = j + 1)
+        begin : block_loop
+            localparam ii = H - i - 1;
+            assign dst[(j * H + ii + 1) * S - 1 : (j * H + ii) * S] = 
+                   src[(i * W + j + 1) * S - 1 : (i * W + j) * S];
+        end
+    end
+endgenerate
+
+endmodule
---
+++ test_benches/layout_tools_test.v
@@ -0,0 +1,57 @@
+module layout_tools_test;
+
+wire[47:0] original = {8'b00000000,
+                     8'b11000011,
+                     8'b11000000,
+                     8'b00011111,
+                     8'b00010000,
+                     8'b00010001};
+
+wire[47:0] swap_v;
+swap_v_layout#(8, 6, 1) v(.src(original), .dst(swap_v));
+wire[47:0] swap_h;
+swap_h_layout#(8, 6, 1) h(.src(original), .dst(swap_h));
+wire[47:0] rotate_l;
+rotate_l_layout#(8, 6, 1) l(.src(original), .dst(rotate_l));
+wire[47:0] rotate_r;
+rotate_r_layout#(8, 6, 1) r(.src(original), .dst(rotate_r));
+
+task print_layout_horizontally(input[127:0] msg, input[47:0] layout);
+    begin
+        $display("%s:\n%b\n%b\n%b\n%b\n%b\n%b\n", msg,
+            layout[47:40], 
+            layout[39:32], 
+            layout[31:24], 
+            layout[23:16], 
+            layout[15:8], 
+            layout[7:0]
+        );
+    end
+endtask
+
+task print_layout_vertically(input[127:0] msg, input[47:0] layout);
+    begin
+        $display("%s:\n%b\n%b\n%b\n%b\n%b\n%b\n%b\n%b\n", msg,
+            layout[47:42], 
+            layout[41:36], 
+            layout[35:30], 
+            layout[29:24], 
+            layout[23:18], 
+            layout[17:12], 
+            layout[11:6], 
+            layout[5:0]
+        );
+    end
+endtask
+
+initial
+begin
+    #1;
+    print_layout_horizontally("Original", original);
+    print_layout_horizontally("swap_v", swap_v);
+    print_layout_horizontally("swap_h", swap_h);
+    print_layout_vertically("rotate_l", rotate_l);
+    print_layout_vertically("rotate_r", rotate_r);
+end
+
+endmodule
