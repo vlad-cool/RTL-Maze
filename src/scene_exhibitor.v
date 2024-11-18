d4eab6a6790dd7c416be4abd442e1fb48eb87629
VoyakaGOD <ab3000645@gmail.com>
2024-11-18 21:45:18 +0300

    fix troubles with walls shift

--- src/scene_exhibitor.v
+++ src/scene_exhibitor.v
@@ -1,7 +1,7 @@
 // by Aleksandr
 // 10x15 tiles scene with 32x32 tiles.
 // Draw walls and food. Should be started after tft_init finished.
-// tft_dc always = 1
+// tft_dc always = 1(DATA)
 
 module scene_exhibitor
 (
@@ -17,14 +17,15 @@ module scene_exhibitor
     output wire busy
 );
 
-localparam SCENE_WIDTH = 320;
-localparam SCENE_HEIGHT = 480;
+localparam HALF_TILE_SIZE = 16;
+localparam MAX_X_INDEX = 320 + HALF_TILE_SIZE;
+localparam MAX_Y_INDEX = 480 + HALF_TILE_SIZE;
 
 reg[8:0] x_index;
 reg[8:0] y_index;
 reg[1:0] color_index;
 
-assign busy = (x_index < SCENE_WIDTH) & (y_index < SCENE_HEIGHT) & enable;
+assign busy = (x_index < MAX_X_INDEX) & (y_index < MAX_Y_INDEX) & enable;
 
 wire[7:0] wall_color[2:0];
 assign wall_color[0] = 8'h3a;
@@ -34,12 +35,12 @@ assign wall_color[2] = 8'hd5;
 wire wall_value;
 wall_layout layout
 (
-    .x(x_index[4:1] + 8), //shift & double size walls grid
-    .y(y_index[4:1] + 8), //shift & double size walls grid
-    .left(h_walls[y_index[8:5] * 11 + x_index[8:5]]),
-    .top(v_walls[y_index[8:5] * 11 + x_index[8:5]]),
-    .right(h_walls[y_index[8:5] * 11 + x_index[8:5] + 1]),
-    .bottom(v_walls[y_index[8:5] * 12 + x_index[8:5]]),
+    .x(x_index[4:1]), //double size walls grid
+    .y(y_index[4:1]), //double size walls grid
+    .left(x_index[8:5] == 0 ? 0 : h_walls[y_index[8:5] * 10 + x_index[8:5] - 1]),
+    .top(y_index[8:5] == 0 ? 0 : v_walls[y_index[8:5] * 11 + x_index[8:5] - 11]),
+    .right(x_index[8:5] == 9 ? 0 : h_walls[y_index[8:5] * 10 + x_index[8:5]]),
+    .bottom(y_index[8:5] == 14 ? 0 : v_walls[y_index[8:5] * 11 + x_index[8:5]]),
     .value(wall_value)
 );
 
@@ -47,7 +48,9 @@ always @(posedge clk)
 begin
     if(rst)
     begin
-        {y_index, x_index, color_index} <= 0;
+        color_index <= 0;
+        x_index <= HALF_TILE_SIZE;
+        y_index <= HALF_TILE_SIZE;
         tft_transmit <= 0;
     end
     else if(color_index == 3)
@@ -55,9 +58,9 @@ begin
         color_index <= 0;
         x_index <= x_index + 1;
     end
-    else if(x_index == SCENE_WIDTH)
+    else if(x_index == MAX_X_INDEX)
     begin
-        x_index <= 0;
+        x_index <= HALF_TILE_SIZE;
         y_index <= y_index + 1;
     end
     else if(busy && (~tft_busy) & (~tft_transmit))
