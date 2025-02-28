4bc0fd83b9fd1050b0d8b4fe4587efafe44c2ba2
VoyakaGOD <ab3000645@gmail.com>
2025-02-28 20:44:47 +0300

    edited README.md

--- README.md
+++ README.md
@@ -16,4 +16,18 @@ To run it just use `make` command in **screen_simulation_2** folder.
 
 ## Synthesis
 
-...
+**Usage of Quartus is expected!**
+
+To synthesize and upload project just use `make TARGET=[name]` in root directory of the project.
+
+This make script creates Maze.qsf with necessary settings and uses pins assignments from **pins_config**.
+
+And then synthesize and upload it via USB Bluster. 
+
+If you want to just create quartus settings file, use `make init TARGET=[name]`.
+
+Available names list:
+
+- **C7** for EP4CE115F29C7
+
+- **C8** for EP4CE6E22C8
--- src/generator.v
+++ src/generator.v
@@ -8,11 +8,11 @@ module generator
     input wire enable,
     input wire[31:0] seed,
 
-    output wire busy;
+    output wire busy,
 
     output wire[159:0] h_walls,
     output wire[164:0] v_walls
-)
+);
 
 reg [134:0] v_walls;
 reg [139:0] h_walls;
@@ -25,10 +25,10 @@ wire[3:0] allowed_directions;
 
 always @(posedge clk) begin
     if (~rst) begin
-        v_walls <= {135{'b1}};
-        h_walls <= {139{'b1}};
+        v_walls <= {135{1'b1}};
+        h_walls <= {139{1'b1}};
 
-        visited <= {0, 149{'b1}};
+        visited <= {1'b0, {149{1'b1}}};
 
         prev_x <= 0;
         prev_y <= 0;
